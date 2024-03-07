8444a12c98064fbc942324e81dab99e67d615a9e
ramiro350 <ramirofilho18@gmail.com>
2024-03-07 11:21:44 -0300

    feat: add column de roles para usuários usando roles do keycloak

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,12 +11,14 @@ class User < ApplicationRecord
   def self.from_omniauth(auth)
     auth = JSON.parse auth.to_json, object_class: OpenStruct
     user = find_or_create_by(uid: auth.uid)
+    excluded_roles = ["default-roles-traineducation", "offline_access", "uma_authorization"]
 
     user.update(
       email: auth.info.email,
       first_name: auth.info.first_name,
       last_name: auth.info.last_name,
       provider: auth.provider,
+      roles: auth.extra.raw_info.realm_access.roles.reject { |role| excluded_roles.include?(role)},
       password: Devise.friendly_token[0, 20]
     )
 
