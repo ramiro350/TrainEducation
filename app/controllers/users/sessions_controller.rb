0a3e0ef07ea437fb6c0a6add0e3fb0884e064f20
ramiro350 <ramirofilho18@gmail.com>
2024-03-07 16:23:56 -0300

    feat: coloca redirect no logout pra página incial

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,6 +4,7 @@ class Users::SessionsController < Devise::SessionsController
     # debugger
     
     destroy
+    # redirect_to home_index_path
     # debugger
     # session.delete(:devise)
     # sign_out current_user
@@ -13,7 +14,7 @@ class Users::SessionsController < Devise::SessionsController
 
   def destroy
     super do
-      return redirect_to url_for("http://localhost:8080/auth/realms/TrainEducation/protocol/openid-connect/logout"), allow_other_host: true
+      return redirect_to url_for("http://localhost:8080/auth/realms/TrainEducation/protocol/openid-connect/logout?post_logout_redirect_uri=http://localhost:3000/&client_id=TrainEducation"), allow_other_host: true
     end
   end
 end
