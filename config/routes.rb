cf1cd95459083138ffb2880e1b1974f172777d9e
ramiro350 <ramirofilho18@gmail.com>
2024-03-15 11:51:09 -0300

    feat: add rota pra treinar o modelo

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     sessions: 'users/sessions'
   }
 
+  get 'train_model', to: 'training#train_model'
   resources :datasets
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_scope :user do
